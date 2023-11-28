83de3ea30b72cfa1d531abd485713d7c230ae45d
Casey Latimere Kreicar <kreicarcl@alumni.vcu.edu>
2023-11-27 20:23:22 -0500

    python basics overview program

---
+++ basicOverview.py
@@ -0,0 +1,25 @@
+#module
+import os
+import fractions
+#define variables
+stringVariable = "name"
+integerVariable = 19
+floatingPointVariable = 0.35
+#define array
+shortList = [7, 8, "casey", .586]
+#variable = user's keyboard input   
+userStringInput = input("input from user string only: \n")
+userIntegerInput = input("input from user integer only: \n")
+#evaluate arithmetic
+arithmetic = (integerVariable + floatingPointVariable) - 1 * 2 ** 4 / 1.55
+#add variable to end of array using method (methods are functions associated with specific objects)
+shortList.append(userStringInput)
+#define variable using information from imported os module
+operatingSystemInformation = os.uname()
+#print to console/terminal shortList[0,1] shows first item in array          \n = escape character and newline
+print("message within program goes here\n", 4, "\n", shortList[0], "\n", arithmetic, "\n", operatingSystemInformation)
+#format float number
+#define function
+#call function
+#while loop
+#for loop
